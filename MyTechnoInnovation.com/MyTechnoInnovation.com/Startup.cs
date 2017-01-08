903ffbfc047d66d5c0a4bc7c88435caadb2f1d5b
Amit Shinde <amitshinde14@gmail.com>
2017-01-08 23:17:02 +0530

    Add MVC project
    Add MVC project

---
+++ MyTechnoInnovation.com/MyTechnoInnovation.com/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyTechnoInnovation.com.Startup))]
+namespace MyTechnoInnovation.com
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ MyTechnoInnovation.com/MyTechnoInnovation.com/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyTechnoInnovation.com.MvcApplication" Language="C#" %>
